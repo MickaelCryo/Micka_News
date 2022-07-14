0e137e4312128a2e16eec1008631e2f73103d0ce
Etilium <67971875+MickaelCryo@users.noreply.github.com>
2022-07-14 09:49:22 +0200

    [Top-Bar] Ajustement title

--- frontend/src/layout/topBar/topBar.jsx
+++ frontend/src/layout/topBar/topBar.jsx
@@ -2,15 +2,15 @@
 
 export default function TopBar() {
   return (
-    <div className="bg-red-600 w-full h-16">
-      <div className="grid grid-cols-2 gap-6 content-center">
+    <div className="bg-red-600 w-full">
+      <div className="grid grid-cols-2 h-16 content-center">
         {/* Left */}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col justify-self-center items-center">
           <h2 className="text-white	font-bold">The Micka News</h2>
           <h6 className="text-white	text-sm">News & Lifestyle Magazine</h6>
         </div>
         {/* Right */}
-        <div className="flex justify-center items-center">
+        <div className="flex justify-self-center items-center">
           <a href="/" className="text-white">
             Login
           </a>
